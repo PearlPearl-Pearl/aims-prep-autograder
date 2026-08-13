00177a11778495f70e6ca9f85e03667d95f67f9b
PearlPearl-Pearl <pksandoh@gmail.com>
2026-08-13 02:19:37 +0200

    Made some changes that were necessary

--- src/test_engine.py
+++ src/test_engine.py
@@ -133,31 +133,3 @@ class TestEngine:
             })
 
         return results
-
-
-# if __name__ == "__main__":
-
-#     engine = TestEngine(
-#         assignments_dir=Path("..", "assignments")
-#     )
-
-#     result = engine.run_tests(
-#         notebook_path=Path(
-#             "..",
-#             "submissions",
-#             "pearl_kuuridong_PP1.ipynb"
-#         ),
-#         assignment_code="PP1"
-#     )
-
-#     for test in result["tests"]:
-#         print(
-#             f"{test['test']}: "
-#             f"{test['earned']}/{test['possible']}"
-#         )
-
-#     print(
-#         f"\nObjective score: "
-#         f"{result['objective_score']}/"
-#         f"{result['total_marks']}"
-#     )
